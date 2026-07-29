fc0ad3ae155b8b6f5410666e8fbd0f114e25a22b
Goutham K <goutham02272006@gmail.com>
2026-07-29 10:21:53 +0530

    fix: move node group to public subnets to fix NodeCreationFailure

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -80,7 +80,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  subnet_ids      = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
   scaling_config {
     desired_size = 2
